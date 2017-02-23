d70ed616e065eaa57c2c1cf8e75afebbf83abfe9
Kirtan Gajjar <kirtangajjar95@gmail.com>
2017-02-23 15:09:03 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-layout id="app">
+      <v-header><h1 class="red-5">My Project</h1 ></v-header>
+      <v-layout>
+        <v-slider>Slider</v-slider>
+        <v-content>Content</v-content>
+      </v-layout>
+      <v-footer>Copyright &copy; All rights reserved</v-footer>
+    </v-layout>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+.bg-primary {
+  background: #555 !important;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #aaa;
+  height: 100%;
+}
+
+.ant-layout-footer {
+  text-align: center;
+}
+</style>
